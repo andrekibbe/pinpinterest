4e2e57d7d15aea983c289d475f752c3e750b734f
Andre Kibbe <akibbe02@gmail.com>
2015-09-14 19:20:10 -0700

    Add show, edit, update, and destroy actions

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,11 @@
 class PinsController < ApplicationController
+  before_action :find_pin, only: [:show, :edit, :update, :destroy]
+
   def index
+    @pins = Pin.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
@@ -8,11 +14,35 @@ class PinsController < ApplicationController
 
   def create
     @pin = Pin.new(pin_params)
+
+    if @pin.save
+      redirect_to @pin, notice: "Successfully created new pin"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: "Pin was successfully updated"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
   end
 
   private
 
     def pin_params
       params.require(:pin).permit(:title, :description)
+    end
+
+    def find_pin
+      @pin = Pin.find(params[:id])
     end
 end
